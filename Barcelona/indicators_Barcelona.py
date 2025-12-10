32cb8a2e9ce3283292cb745b8ae59085b9da0611
Eleftheriaexarchou <125801519+Eleftheriaexarchou@users.noreply.github.com>
2025-12-10 10:33:24 +0100

    Modify baseline years and add hot day/night functions
    Updated baseline years for drought-related functions and added new functions for hot days and nights.

--- Barcelona/indicators_Barcelona.py
+++ Barcelona/indicators_Barcelona.py
@@ -19,7 +19,7 @@ def cdd(pr, thresh='1 mm/day', freq='YS'):
                                                                 resample_before_rl=True)
     return indicator
     
-def drought_spi(tp_daily, baseline = (1990, 2006), scale=3):
+def drought_spi(tp_daily, baseline = (1981, 2011), scale=3):
         from climate_indices import indices as cindx
         """ Using Standard Precipitation index """
 
@@ -74,9 +74,15 @@ def drought_spi(tp_daily, baseline = (1990, 2006), scale=3):
         return  stand_prec_index
     
 def spi3(pr, scale = 3):
-    baseline = (1990, 2006)
+    baseline = (1981, 2011)
     spi = drought_spi(pr, scale=scale, baseline = baseline)
     return spi
+    
+def spi12(pr, scale = 12):
+    baseline = (1981, 2011)
+    spi = drought_spi(pr, scale=scale, baseline = baseline)
+    return spi
+
 
 def drought_spei(daily_water_budget, baseline = (1981, 2011), scale=3):
         from climate_indices import indices as cindx
@@ -129,7 +135,7 @@ def spei3(pr, tas, scale = 3):
     pr_mon = pr.resample(time='MS').sum(dim = 'time') 
     dwb = pr - pet
     dwb = dwb.to_dataset(name = 'DWsB')
-    baseline = (1989, 2018)
+    baseline = (1981, 2011)
     
     spei = drought_spei(dwb, scale=scale, baseline = baseline)
     return spei
@@ -152,41 +158,81 @@ def txx(tasmax, freq = 'seas', season = ''):
     txx.attrs['units'] = 'degC'
 
     return txx
-#def txx(tasmax, freq = 'seas', season = ''):
-#    tasmax.attrs['units'] = 'degk'
-#    if freq == 'seas':
-#        txx = xclim.indices.tx_max(tasmax, freq='QS-DEC').groupby('time.season').sel(season = season)
-#    else:
-#        txx = xclim.indices.tx_max(tasmax, freq='YE')
-#    txx.attrs['units'] = 'degK'
 
-#    return txx
 
-#    else:
-#        txx = xclim.indices.tx_max(tasmax, freq='YE')
-#    txx.attrs['units'] = 'degC'
-
-#    return txx
-
-def txx(tasmax, freq = 'seas', season = ''):
+def hot_days_30(tasmax, freq = 'seas', season = ''):
     tasmax.attrs['units'] = 'degC'
+    
     if freq == 'seas':
-        # txx = xclim.indices.tx_max(tasmax, freq='QS-DEC').groupby('time.season').sel(season = season)
-        # Get seasonal maxima
-        txx = xclim.indices.tx_max(tasmax, freq='QS-DEC')        
-        # Then group by season and reduce, e.g. take the max across each season
-        #txx = txx.groupby('time.season').max()
-        txx = txx.sel(time=txx['time.season'] == season)
+        hd30 = xclim.indices.tx_days_above(tasmax, thresh='30 degC', freq='QS-DEC')
+        if season:
+            hd30 = hd30.sel(time=hd30['time.season'] == season)
+    else:  # annual
+        hd30 = xclim.indices.tx_days_above(tasmax, thresh='30 degC', freq='YS')
+    
+    hd30.attrs['units'] = 'days'
+    return hd30
+    
+
+def hot_nights_20(tasmin, freq = 'seas', season = ''):
+    tasmin.attrs['units'] = 'degC'
+    
+    if freq == 'seas':
+        hn20 = xclim.indices.tn_days_above(tasmin, thresh='20 degC', freq='QS-DEC')
+        if season:
+            hn20 = hn20.sel(time=hn20['time.season'] == season)
+    else:  # annual
+        hn20 = xclim.indices.tn_days_above(tasmin, thresh='20 degC', freq='YS')
+    
+    hn20.attrs['units'] = 'days'
+    return hn20
+    
 
 
-        # Now you can select one season
-        #txx = txx.sel(season=season)
-    else:
-        txx = xclim.indices.tx_max(tasmax, freq='YE')
-    txx.attrs['units'] = 'degC'
+def hot_nights_25(tasmin, freq = 'seas', season = ''):
+    tasmin.attrs['units'] = 'degC'
+    
+    if freq == 'seas':
+        hn25 = xclim.indices.tn_days_above(tasmin, thresh='25 degC', freq='QS-DEC')
+        if season:
+            hn25 = hn25.sel(time=hn25['time.season'] == season)
+    else:  # annual
+        hn25 = xclim.indices.tn_days_above(tasmin, thresh='25 degC', freq='YS')
+    
+    hn25.attrs['units'] = 'days'
+    return hn25
+    
 
-    return txx
 
+def hot_nights_30(tasmin, freq = 'seas', season = ''):
+    tasmin.attrs['units'] = 'degC'
+    
+    if freq == 'seas':
+        hn30 = xclim.indices.tn_days_above(tasmin, thresh='30 degC', freq='QS-DEC')
+        if season:
+            hn30 = hn30.sel(time=hn30['time.season'] == season)
+    else:  # annual
+        hn30 = xclim.indices.tn_days_above(tasmin, thresh='30 degC', freq='YS')
+    
+    hn30.attrs['units'] = 'days'
+    return hn30
+    
+
+
+def hot_days_35(tasmax, freq = 'seas', season = ''):
+    tasmax.attrs['units'] = 'degC'
+    
+    if freq == 'seas':
+        hd35 = xclim.indices.tx_days_above(tasmax, thresh='35 degC', freq='QS-DEC')
+        if season:
+            hd35 = hd35.sel(time=hd30['time.season'] == season)
+    else:  # annual
+        hd35 = xclim.indices.tx_days_above(tasmax, thresh='35 degC', freq='YS')
+    
+    hd35.attrs['units'] = 'days'
+    return hd35
+    
+# xclim.indicators.atmos.hot_days(tasmax='tasmax', *, thresh='25 degC', freq='YS', ds=None, **indexer) 
 
 def heatwave_length(tx, tn, freq='YS'):
     tx.attrs['units'] = 'degC'
